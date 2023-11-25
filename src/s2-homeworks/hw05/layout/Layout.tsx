77b7571ba83c2d114888f3bb8985871352ae696f
Aleksandr <sky7way@yandex.ru>
2023-11-25 19:28:36 +0300

    HW-5

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -1,12 +1,12 @@
-import React, {FC, ReactNode, useEffect, useState} from 'react'
-import {Header} from '../header/Header'
-import {Sidebar} from '../sidebar/Sidebar'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
+import { Header } from '../header/Header'
+import { Sidebar } from '../sidebar/Sidebar'
 
 type PropsType = {
     children: ReactNode
 }
 
-export const Layout: FC<PropsType> = ({children}) => {
+export const Layout: FC<PropsType> = ({ children }) => {
     const [open, setOpen] = useState(false)
     const handleClose = () => setOpen(false)
     const handleOpen = () => setOpen(true)
@@ -18,16 +18,10 @@ export const Layout: FC<PropsType> = ({children}) => {
 
     return (
         <>
-            {open ? (
-                <Sidebar open={open} handleClose={handleClose}/>
-            ) : (
-                <Header handleOpen={handleOpen}/>
-            )}
-
-            {/*<Sidebar open={open} handleClose={handleClose}/>*/}
-            {/*<Header handleOpen={handleOpen}/>*/}
-
+            <Sidebar open={open} handleClose={handleClose} />
+            <Header handleOpen={handleOpen} />
             <div>
+                {/*страницы*/}
                 {children}
             </div>
         </>
